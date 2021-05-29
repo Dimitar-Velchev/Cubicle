0eb374992c468f096f11fd7ec6c80b45b0f70d91
Dimitar <dimitarvelchev59@gmail.com>
2021-05-29 22:21:07 +0300

    express config

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const express = require("express");
-const handlebars = require("express-handlebars");
 const config = require("./config/config");
-
+const expressConfig = require('./config/express')
 const app = express();
-app.engine(
-  "hbs",
-  handlebars({
-    extname: "hbs",
-  })
-);
-app.use(express.static('public'))
 
-app.set("view engine", "hbs");
+expressConfig(app)
 
 app.get("/", (req, res) => {
   res.render('home',{layout: false})
