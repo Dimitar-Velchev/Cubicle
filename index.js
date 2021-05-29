979435630f8f4c136d0a325384b8b904a632ce92
Dimitar <dimitarvelchev59@gmail.com>
2021-05-29 22:03:21 +0300

    home page setup

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const express = require('express');
-
-const config = require('./config/config');
+const express = require("express");
+const handlebars = require("express-handlebars");
+const config = require("./config/config");
 
 const app = express();
+app.engine(
+  "hbs",
+  handlebars({
+    extname: "hbs",
+  })
+);
 
+app.set("view engine", "hbs");
 
-app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}`))
+app.get("/", (req, res) => {
+  res.render('home',{layout: false})
+});
+
+app.listen(config.PORT, () =>
+  console.log(`Server is running on port ${config.PORT}`)
+);
