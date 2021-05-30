8c3591d16e62ceb52b2fee3e22ff27acb0ff14da
Dimitar <dimitarvelchev59@gmail.com>
2021-05-30 15:51:33 +0300

    Add Titles to layout and create routes

--- routes.js
+++ routes.js
@@ -1,10 +1,14 @@
-const {Router} = require('express');
+const { Router } = require("express");
+
+const productController = require("./controllers/productController");
+const aboutController = require("./controllers/aboutController");
 
 const router = Router();
 
+router.use("/", productController);
+router.use("/about", aboutController);
+router.get('*', (req, res) => {
+  res.render("404");
+});
 
-router.get("/", (req, res) => {
-    res.render('home',{layout: false})
-  });
-  
-  module.exports = router;
+module.exports = router;
