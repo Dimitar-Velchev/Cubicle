39064c9985e4b65ee12552cbd7392ec42026b207
Dimitar <dimitarvelchev59@gmail.com>
2021-06-03 13:15:36 +0300

    Add database json file

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,23 +1,24 @@
 const { Router } = require("express");
-
+const productServece = require("../services/productService");
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render("home", {title: 'Browse'});
+  res.render("home", { title: "Browse" });
 });
 
 router.get("/create", (req, res) => {
-  res.render("create", { title: 'Create'});
+  res.render("create", { title: "Create" });
 });
 
-router.post('/create', (req, res)=>{
-    console.log('created');
-    res.send('created');
-})
+router.post("/create", (req, res) => {
+  //TODO: validate inputs!!!
+  productServece.create(req.body);
+  res.redirect("/products");
+});
 
-router.get('/details/:productId',(req,res)=>{
-    console.log(req.params.productId);
-    res.render('details', {title: 'Product Details'})
-})
+router.get("/details/:productId", (req, res) => {
+  console.log(req.params.productId);
+  res.render("details", { title: "Product Details" });
+});
 
 module.exports = router;
